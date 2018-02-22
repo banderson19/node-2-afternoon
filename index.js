446faca948e54b7453a879b67d7d6dca62d9f2ac
banderson19 <brad.anderson019@gmail.com>
2018-02-21 22:44:35 -0700

    created http request methods

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ const cors = require('cors');
 const massive = require('massive');
 require('dotenv').config();
 
+const products_controller = require('./products_controller');
+
 const app = express();
 app.use( bodyParser.json() );
 app.use( cors() );
+
+
 
 massive(process.env.CONNECTION_STRING).then(dbInstance => {
     app.set('db', dbInstance)
 })
 
+app.get('/api/products', products_controller.getAll);
+app.get('/api/prodcuts/:id', products_controller.getOne);
+app.put('/api/products/:id', products_controller.update);
+app.post('/api/products', products_controller.create);
+app.delete('/api/products', products_controller.delete);
 
 const port = process.env.PORT || 3000;
 app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
